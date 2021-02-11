129cc8938aab2ab009d48dd776005ad7649f9fe5
Fatima Rizvi <fatimarzv23@gmail.com>
2021-02-10 20:50:27 -0600

    Created conditional to check if ship was hit

--- battleship.py
+++ battleship.py
@@ -20,3 +20,9 @@ ship_col = random_col(board)
 
 guess_row = int(input("Guess row: "))
 guess_col = int(input("Guess col: "))
+
+print(ship_col)
+print(ship_row)
+
+if guess_row == ship_row and guess_col == ship_col:
+    print("Argh! You sank my battle ship!")
