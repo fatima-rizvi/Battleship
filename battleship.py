b31ab558499357eed1de17b262513e98f17236d6
Fatima Rizvi <fatimarzv23@gmail.com>
2021-02-10 22:15:18 -0600

    Finished the select_diff function

--- battleship.py
+++ battleship.py
@@ -21,20 +21,29 @@ ship_col = random_col(board)
 print(f"Row num: {ship_row + 1}")
 print(f"Col num: {ship_col + 1}")
 
-def select_diff():    # FUnction to select difficulty level, which affects number of turns
-    choice = int(input("""
-    ----------------------------
-              BATTLESHIP                
+def select_diff():    # Function to select difficulty level, which affects number of turns
+    all_turns = { 1: 25, 2: 10, 3: 5, 4: 3, 5: 1 }
+    
+    choice = " "
+    while choice not in all_turns:
+        choice = int(input("""
+        ----------------------------
+                BATTLESHIP                
 
-         Select difficulty:        
+            Select difficulty:        
 
-       1. Fun      (25 turns) 
-       2. Easy     (10 turns)
-       3. Normal    (5 turns)
-       4. Hard      (3 turns) 
-       5. Expert     (1 turn)    
-    ----------------------------
-    Difficulty (1/2/3/4/5): """))
+        1. Fun      (25 turns) 
+        2. Easy     (10 turns)
+        3. Normal    (5 turns)
+        4. Hard      (3 turns) 
+        5. Expert     (1 turn)    
+        ----------------------------
+        Difficulty (1/2/3/4/5): """))
+        if choice in all_turns:
+            return all_turns[choice]
+        else:
+            print("Sorry, that is not an option. Please try again.")
+
 
 
 turns = 4 # Allow the player 4 turns to hit the ship. Create an input to select ifficulty level with more or less turns.
