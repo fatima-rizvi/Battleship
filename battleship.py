6769ef6f598acfccb57e1a04d84b627bdb461a90
Fatima Rizvi <fatimarzv23@gmail.com>
2021-02-10 21:51:46 -0600

    Fixed conditionals to account for incorrect input and adjusted inputs to account for index pos starting at 0

--- battleship.py
+++ battleship.py
@@ -25,23 +25,23 @@ turns = 4 # Allow the player 4 turns to hit the ship. Create an input to select 
 for turn in range(turns):
     print(f"\nTurn #{turn}")
 
-    guess_row = int(input("Guess row: "))
-    guess_col = int(input("Guess col: "))
+    guess_row = int(input("Guess row (1 - 5): ")) - 1 # Minus one accounts for index position starting at 0
+    guess_col = int(input("Guess col (1 - 5): ")) - 1
 
 
-    if board[guess_row][guess_col] == "X":
-        print("You already guessed that one")
-    elif guess_row == ship_row and guess_col == ship_col:
-        print("Argh! You sank my battleship! You win!")
-        break
-    else: 
-        if guess_row not in range(5) or guess_col not in range(5):
-            print("That spot isn't in the ocean! What are you playing at?")
-        else:
+    if guess_row in range(5) and guess_col in range(5):
+        if board[guess_row][guess_col] == "X":
+            print("You already guessed that one")
+        elif guess_row == ship_row and guess_col == ship_col:
+            print("Argh! You sank my battleship! You win!")
+            break
+        else: 
             print("Ha! You missed my battleship!")
             board[guess_row][guess_col] = "X"
             print_board(board)
 
-        if turn == turns:
-            print("You lost! Better luck next time!")
+            if turn == turns:
+                print("You lost! Better luck next time!")
+    else:
+        print("That spot isn't in the ocean! What are you playing at?")
 
