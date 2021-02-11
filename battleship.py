2f73291d67e3bea932d61bc40c020ad4f9c2d7cf
Fatima Rizvi <fatimarzv23@gmail.com>
2021-02-10 20:46:34 -0600

    hid battleships in random locations

--- battleship.py
+++ battleship.py
@@ -1,10 +1,19 @@
+from random import randint
+
 board = []
 
-for i in range(0, 5):
+for x in range(0, 5):
     board.append(["O"] * 5)
 
 def print_board(board):
-    for i in board:
-        print(" ".join(i))
+    for row in board:
+        print(" ".join(row))
 
-print_board(board)
+def random_row(board):
+    return randint(0, len(board) -1)
+
+def random_col(board):
+    return randint(0, len(board) -1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
